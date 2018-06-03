1ecdb5a9266cfe4d828bb77d8efeb0aab54d8be3
Tariod <wasd1299@gmail.com>
2018-06-03 18:18:32 +0300

    Implementation of searching in the region.

--- lib/Rtree.py
+++ lib/Rtree.py
@@ -7,7 +7,19 @@ class Point:
         self.y = y
 
 
-class Rectangul:
+class Circle:
+    def __init__(self, centre, radius):
+        self.centre = centre
+        self.radius = radius
+
+    def isInside(self, rectangle):
+        x = (rectangle.rightLower.x + rectangle.leftUpper.x) / 2
+        y = (rectangle.leftUpper.y + rectangle.rightLower.y) / 2
+        distance = math.sqrt(math.pow(x - self.centre.x, 2) + math.pow(y - self.centre.y, 2))
+        return distance <= self.radius
+
+
+class Rectangle:
     def __init__(self, leftUpper, rightLower):
         self.leftUpper = Point(leftUpper.x, leftUpper.y)
         self.rightLower = Point(rightLower.x, rightLower.y)
@@ -16,8 +28,8 @@ class Rectangul:
         return (self.rightLower.x - self.leftUpper.x) * (self.rightLower.y - self.leftUpper.y)
 
     def increaseArea(self, service):
-        x = math.fabs(min(self.rightLower.x, service.rightLower.x) - max(self.leftUpper.x, service.leftUpper.x))
-        y = math.fabs(min(self.rightLower.y, service.rightLower.y) - max(self.leftUpper.y, service.leftUpper.y))
+        x = min(self.rightLower.x, service.rightLower.x) - max(self.leftUpper.x, service.leftUpper.x)
+        y = min(self.rightLower.y, service.rightLower.y) - max(self.leftUpper.y, service.leftUpper.y)
         if x == 0:
             return y * (self.rightLower.x - self.leftUpper.x)
         if y == 0:
@@ -40,19 +52,34 @@ class Rectangul:
             changed = True
         return changed
 
+    def _isInside(self, obj):
+        return self.leftUpper.x <= obj.x <= self.rightLower.x and self.rightLower.y <= obj.y <= self.leftUpper.y
 
-class Service(Rectangul):
+    def isOverlaps(self, obj):
+        if self._isInside(obj.leftUpper) or self._isInside(obj.rightLower):
+            return True
+
+        leftLowerObj = Point(obj.leftUpper.x, obj.rightLower.y)
+        if self._isInside(leftLowerObj):
+            return True
+        rightUpperObj = Point(obj.rightLower.x, obj.leftUpper.y)
+        if self._isInside(rightUpperObj):
+            return True
+        return False
+
+
+class Service(Rectangle):
     def __init__(self, coordinate, type, subtype, name, address):
-        Rectangul.__init__(self, coordinate, coordinate)
+        Rectangle.__init__(self, coordinate, coordinate)
         self.type = type
         self.subtype = subtype
         self.name = name
         self.address = address
 
 
-class Node(Rectangul):
+class Node(Rectangle):
     def __init__(self, obj):
-        Rectangul.__init__(self, obj.leftUpper, obj.rightLower)
+        Rectangle.__init__(self, obj.leftUpper, obj.rightLower)
         self.parent = None
 
     def updateParentBound(self):
@@ -98,6 +125,32 @@ class Rtree:
         self.max = max
         self.min = math.ceil(max * 0.4)
         self.root = None
+
+    def find(self, position, radius):
+        region = Circle(position, radius)
+        regionRect = Rectangle(Point(position.x - radius, position.y + radius), Point(position.x + radius, position.y - radius))
+        result = self._find(self.root, regionRect)
+        for service in result:
+            if region.isInside(service) is False:
+                result.remove(service)
+            else:
+                print("=====================")
+                print("Name:", service.name)
+                print("Type:", service.type)
+                print("Subtype:", service.subtype)
+                print("Address:", service.address)
+
+    def _find(self, root, region):
+        res = []
+        if isinstance(root, NodeLeaf):
+            for service in root.services:
+                if region.isOverlaps(service):
+                    res.append(service)
+        else:
+            for child in root.children:
+                if child.isOverlaps(region) or region.isOverlaps(child):
+                    res.extend(self._find(child, region))
+        return res
 
     def insert(self, obj):
         if self.root is None:
