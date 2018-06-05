557c4d68f56d912f40fda4dd7de303a07bf0bcdf
Mykyta Yukhta <mykyta.yukhta@yahoo.com>
2018-06-06 02:51:50 +0300

    Add argument parser support

--- what-is-near.py
+++ what-is-near.py
@@ -1,23 +1,34 @@
+import io
 import sys
+import argparse
+
 sys.path.append('./lib/')
 from Rtree import Rtree
 from Point import Point
 from Service import Service
 
 
+parser = argparse.ArgumentParser()
+parser.add_argument("db", help="database file to get map info from", type=str)
+parser.add_argument("lat", help="latitude of a place", type=float)
+parser.add_argument("long", help="longitude of a place", type=float)
+parser.add_argument("size", help="size of a sector to search in", type=float)
+args = parser.parse_args()
+
+
 def parse(fileNme):
-    with open(fileNme) as file:
+    with io.open(fileNme, encoding='utf-8') as file:
         data = file.read().split('\n')
         data = [field.split(';') for field in data[:-1]]
     return data
 
 
 rtree = Rtree(5)
-data = parse('./data/ukraine.csv')
+data = parse('./data/' + args.db)
 for service in data:
     rtree.insert(Service(Point(float(service[0]), float(service[1])), service[2], service[3], service[4], service[5]))
 print('Rtree built')
-res = rtree.find(Point(50.448000, 30.451600), 200)
+res = rtree.find(Point(args.lat, args.long), args.size)
 for service in res:
     print('=====================')
     print('Name:', service.name)
